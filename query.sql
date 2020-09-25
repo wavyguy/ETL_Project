6a63e00ad029e00182d0ed3f90de49366dc24891
wavyguy <66179374+wavyguy@users.noreply.github.com>
2020-09-24 20:34:28 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,12 @@
 -- Query to check successful load
 
-SELECT * FROM players;
+SELECT * FROM nbaplayers;
 
-SELECT * FROM teams;
+SELECT * FROM nbateams;
 
 -- Join tables on team_name
 
-SELECT players.player_name, players.player_salary, teams.team_worth
-FROM players
-INNER JOIN teams
-ON players.team_name = teams.team_name;
+SELECT nbaplayers.player_name, nbaplayers.player_salary, nbateams.team_worth
+FROM nbaplayers
+INNER JOIN nbateams
+ON nbaplayers.team_name = nbateams.team_name;
